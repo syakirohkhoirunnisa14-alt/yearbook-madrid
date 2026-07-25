bec4403dc90f65800894a9ec7d5daa7de40e6a47
Syakiroh Khoirunnsa <syakirohkhoirunnisa14@gmail.com>
2026-07-25 16:02:05 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,23 @@
+const studentsData = [
+    {
+        id: 1,
+        name: "Ahmad Rizky",
+        nickname: "Rizky",
+        photo: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=400&auto=format&fit=crop",
+        quote: "Terus melangkah meski pelan, yang penting tidak jalan di tempat.",
+        ttl: "Jakarta, 12 Januari 2007",
+        hobi: "Futsal, Bermain Gitar",
+        pesan: "Sukses untuk kita semua di masa depan!"
+    },
+    {
+        id: 2,
+        name: "Zah Iput",
+        nickname: "Iput",
+        photo: "https://images.unsplash.com/photo-1517841905240-472988babdf9?q=80&w=400&auto=format&fit=crop",
+        quote: "Masa depan milik mereka yang percaya pada indahnya impian.",
+        ttl: "Bandung, 05 Maret 2007",
+        hobi: "Membaca, Menulis",
+        pesan: "Terima kasih atas 3 tahun kenangan yang indah ini guys!"
+    }
+    // ... Kamu bisa menambahkan hingga 33 siswa di sini
+];
